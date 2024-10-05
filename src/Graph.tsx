18252134c04a84094ee70cdfd39e416cc17e6ac1
Sanjana <arunsanjana3@gmail.com>
2024-10-05 13:54:44 +0530

    Made the necessary changes required in task 2

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -46,22 +46,23 @@ class Graph extends Component<IProps, {}> {
     }
     if (this.table) {
       // Load the `table` in the `<perspective-viewer>` DOM reference.
-
-      // Add more Perspective configurations here.
       elem.load(this.table);
+      // Add more Perspective configurations here.
+
       elem.setAttribute('view','y_line');
       elem.setAttribute('column-pivots','["stock"]');
       elem.setAttribute('row-pivots','["timestamp"]');
       elem.setAttribute('columns','["top_ask_price"]');
-      elem.setAttribute('aggregates',
-             {"stock":"distinct count",
+      elem.setAttribute("aggregates", `
+            {"stock":"distinct count",
               "top_ask_price":"avg",
               "top_bid_price":"avg",
-              "timestamp":"distinct count"});
+              "timestamp":"distinct count"}`);
+
+
     }
   }
-
-  componentDidUpdate() {
+  componentDidUpdate(){
     // Everytime the data props is updated, insert the data into Perspective table
     if (this.table) {
       // As part of the task, you need to fix the way we update the data props to
